11b9c03cec4d8129ddb0c7d1c406d6326086709e
PawelCZ1 <pawelczapran@gmail.com>
2025-04-11 00:10:27 +0200

    Backend added to project

---
+++ backend/src/main/kotlin/project/collectors_hub/CollectorsHubApplication.kt
@@ -0,0 +1,11 @@
+package project.collectors_hub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CollectorsHubApplication
+
+fun main(args: Array<String>) {
+	runApplication<CollectorsHubApplication>(*args)
+}
---
+++ backend/src/test/kotlin/project/collectors_hub/CollectorsHubApplicationTests.kt
@@ -0,0 +1,13 @@
+package project.collectors_hub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CollectorsHubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
